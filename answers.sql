d61327e8b647323697c91c4ad74480738363a8bf
vinothini149 <vinothiniav1409@gmail.com>
2026-09-22 15:17:39 +0530

    Program 15

--- answers.sql
+++ answers.sql
@@ -1,1 +1,41 @@
+SET SERVEROUTPUT ON;
 
+DECLARE
+    marks NUMBER;
+BEGIN
+    marks := 35;
+    IF marks >= 40 THEN
+        DBMS_OUTPUT.PUT_LINE('PASS');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('FAIL');
+    END IF;
+
+    marks := 40;
+    IF marks >= 40 THEN
+        DBMS_OUTPUT.PUT_LINE('PASS');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('FAIL');
+    END IF;
+
+    marks := 75;
+    IF marks >= 40 THEN
+        DBMS_OUTPUT.PUT_LINE('PASS');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('FAIL');
+    END IF;
+
+    marks := 20;
+    IF marks >= 40 THEN
+        DBMS_OUTPUT.PUT_LINE('PASS');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('FAIL');
+    END IF;
+
+    marks := 100;
+    IF marks >= 40 THEN
+        DBMS_OUTPUT.PUT_LINE('PASS');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('FAIL');
+    END IF;
+END;
+/
